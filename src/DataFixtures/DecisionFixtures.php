1dcd73b79c966cbe6f4e007a5cb73c05a5b47b07
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-06-08 12:19:08 +0200

    reviewed

--- src/DataFixtures/DecisionFixtures.php
+++ src/DataFixtures/DecisionFixtures.php
@@ -9,11 +9,13 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class DecisionFixtures extends Fixture
 {
+    public const DECISION_NUMBER = 12;
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
 
-        for ($i = 0; $i <= 12; $i++) {
+        for ($i = 0; $i < self::DECISION_NUMBER; $i++) {
             $decision = new Decision();
             $decision->setTitle($faker->sentence());
             $decision->setStartDate($faker->dateTime());
