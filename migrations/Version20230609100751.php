2aa2bf836ce0bfd5ece14240b1bd4c3ed698e9c3
herve <herveperel@gmail.com>
2023-06-14 11:37:31 +0200

    S02US40

--- migrations/Version20230609100751.php
+++ migrations/Version20230609100751.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230609092639 extends AbstractMigration
+final class Version20230609100751 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,6 +20,7 @@ final class Version20230609092639 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE status (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE decision ADD status_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE decision ADD CONSTRAINT FK_84ACBE486BF700BD FOREIGN KEY (status_id) REFERENCES status (id)');
         $this->addSql('CREATE INDEX IDX_84ACBE486BF700BD ON decision (status_id)');
@@ -29,6 +30,7 @@ final class Version20230609092639 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE decision DROP FOREIGN KEY FK_84ACBE486BF700BD');
+        $this->addSql('DROP TABLE status');
         $this->addSql('DROP INDEX IDX_84ACBE486BF700BD ON decision');
         $this->addSql('ALTER TABLE decision DROP status_id');
     }
