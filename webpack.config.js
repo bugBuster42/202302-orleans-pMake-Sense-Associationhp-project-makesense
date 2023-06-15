b87c11d58795048f09166a29dd4fab21685bd0ea
thomaslavalade <thomaslavalade@gmail.com>
2023-06-15 14:27:52 +0200

    admin tableau user

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addStyleEntry('login', './assets/styles/login.scss')
+    .addStyleEntry('admin', './assets/styles/admin.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
