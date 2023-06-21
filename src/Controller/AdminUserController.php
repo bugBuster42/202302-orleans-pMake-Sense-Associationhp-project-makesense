cdad3b8e1a3dc350a3abe6115effcf010b74753c
thomaslavalade <thomaslavalade@gmail.com>
2023-06-21 16:10:33 +0200

    S03US35_fix changed

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -6,11 +6,13 @@ use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/admin/user')]
+#[IsGranted('ROLE_ADMIN')]
 class AdminUserController extends AbstractController
 {
     #[Route('/', name: 'app_admin_user_index', methods: ['GET'])]
