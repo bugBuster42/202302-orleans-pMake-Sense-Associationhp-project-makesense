e7050549b0424db7fbdebbacd91d1d69e56d5037
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-06-21 15:51:25 +0200

    category view ok

--- src/Controller/AdminCategoryController.php
+++ src/Controller/AdminCategoryController.php
@@ -10,14 +10,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-#[Route('/category')]
-class CategoryController extends AbstractController
+#[Route('/admin/user', name: 'admin_')]
+#[IsGranted('ROLE_ADMIN')]
+class AdminCategoryController extends AbstractController
 {
-    #[Route('/', name: '_index', methods: ['GET'])]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(CategoryRepository $categoryRepository): Response
     {
-        return $this->render('category/index.html.twig', [
+        return $this->render('admin_category/index.html.twig', [
             'categories' => $categoryRepository->findAll(),
         ]);
     }
