d184dec2794fd253bf0463b4e4b6eb7af1edebc2
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-06-22 13:25:25 +0200

    category edit ok

--- src/Controller/AdminCategoryController.php
+++ src/Controller/AdminCategoryController.php
@@ -12,15 +12,33 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-#[Route('/admin/user', name: 'admin_')]
+#[Route('/admin/categorie', name: 'admin_')]
 #[IsGranted('ROLE_ADMIN')]
 class AdminCategoryController extends AbstractController
 {
-    #[Route('/', name: 'index', methods: ['GET'])]
+    #[Route('/', name: 'category_index', methods: ['GET'])]
     public function index(CategoryRepository $categoryRepository): Response
     {
-        return $this->render('admin_category/index.html.twig', [
+        return $this->render('admin/admin_category/index.html.twig', [
             'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'category_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Category $category, CategoryRepository $categoryRepository): Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $categoryRepository->save($category, true);
+
+            return $this->redirectToRoute('category_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('/admin/admin_category/edit.html.twig', [
+            'category' => $category,
+            'form' => $form,
         ]);
     }
 }
