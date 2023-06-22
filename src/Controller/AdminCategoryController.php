90630fce95339857b8189fba42b878171e2417f6
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-06-22 13:45:37 +0200

    category delete ok

--- src/Controller/AdminCategoryController.php
+++ src/Controller/AdminCategoryController.php
@@ -12,15 +12,25 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-#[Route('/admin/user', name: 'admin_')]
+#[Route('/admin/categorie', name: 'admin_')]
 #[IsGranted('ROLE_ADMIN')]
 class AdminCategoryController extends AbstractController
 {
     #[Route('/', name: 'index', methods: ['GET'])]
     public function index(CategoryRepository $categoryRepository): Response
     {
-        return $this->render('admin_category/index.html.twig', [
+        return $this->render('admin/admin_category/index.html.twig', [
             'categories' => $categoryRepository->findAll(),
         ]);
+    }
+
+    #[Route('/{id}', name: 'category_delete', methods: ['POST'])]
+    public function delete(Request $request, Category $category, CategoryRepository $categoryRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $category->getId(), $request->request->get('_token'))) {
+            $categoryRepository->remove($category, true);
+        }
+
+        return $this->redirectToRoute('admin/admin_category_index', [], Response::HTTP_SEE_OTHER);
     }
 }
