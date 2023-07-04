8999a1f6676b013e6f8d61513122f087d8c7fee0
herve <herveperel@gmail.com>
2023-07-04 16:37:12 +0200

    workflow

--- migrations/Version20230704143612.php
+++ migrations/Version20230704143612.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230704143103 extends AbstractMigration
+final class Version20230704143612 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,14 +23,14 @@ final class Version20230704143103 extends AbstractMigration
         $this->addSql('ALTER TABLE decision DROP FOREIGN KEY FK_84ACBE486BF700BD');
         $this->addSql('DROP TABLE status');
         $this->addSql('DROP INDEX IDX_84ACBE486BF700BD ON decision');
-        $this->addSql('ALTER TABLE decision DROP status_id');
+        $this->addSql('ALTER TABLE decision ADD current_place VARCHAR(255) NOT NULL, DROP status_id');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE status (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, code VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE decision ADD status_id INT DEFAULT NULL');
+        $this->addSql('CREATE TABLE status (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE decision ADD status_id INT DEFAULT NULL, DROP current_place');
         $this->addSql('ALTER TABLE decision ADD CONSTRAINT FK_84ACBE486BF700BD FOREIGN KEY (status_id) REFERENCES status (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
         $this->addSql('CREATE INDEX IDX_84ACBE486BF700BD ON decision (status_id)');
     }
