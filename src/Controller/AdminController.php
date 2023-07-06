d742926acc0a92877564c08f6f8cb5ab131877b9
thomaslavalade <thomaslavalade@gmail.com>
2023-07-06 16:14:36 +0200

    admin_search_button

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,13 +4,14 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Form\UserSearchType;
 use App\Form\UserStatusType;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin', name: 'admin_')]
 #[IsGranted('ROLE_ADMIN')]
@@ -26,57 +27,19 @@ class AdminController extends AbstractController
     #[Route('/user', name: 'user_index', methods: ['GET', 'POST'])]
     public function userIndex(UserRepository $userRepository, Request $request): Response
     {
-        $users = $userRepository->findBy([], ['lastname' => 'ASC']);
+        $form = $this->createForm(UserSearchType::class);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $users = $userRepository->searchUser($data['search']);
+        } else {
+            $users = $userRepository->findBy([], ['lastname' => 'ASC']);
+        }
         return $this->render('admin/admin_user/index.html.twig', [
             'users' => $users,
-        ]);
-    }
-
-    #[Route('/user/new', name: 'user_new', methods: ['GET', 'POST'])]
-    public function userNew(Request $request, UserRepository $userRepository): Response
-    {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->save($user, true);
-
-            return $this->redirectToRoute('admin_user_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('admin/admin_user/new.html.twig', [
-            'user' => $user,
             'form' => $form,
         ]);
-    }
-
-    #[Route('/user/{id}/edit', name: 'user_edit', methods: ['GET', 'POST'])]
-    public function userEdit(Request $request, User $user, UserRepository $userRepository): Response
-    {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->save($user, true);
-
-            return $this->redirectToRoute('admin_user_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('admin/admin_user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/user/{id}', name: 'user_delete', methods: ['POST'])]
-    public function userDelete(Request $request, User $user, UserRepository $userRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
-            $userRepository->remove($user, true);
-        }
-
-        return $this->redirectToRoute('admin_user_index', [], Response::HTTP_SEE_OTHER);
     }
 }
