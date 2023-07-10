3687ec24ca5abe470b7a95bbd5e95f77838b15c1
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-07-10 20:39:43 +0200

    img ok

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\HttpFoundation\File\File;
 
 class CategoryFixtures extends Fixture
 {
@@ -18,13 +19,55 @@ class CategoryFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        foreach (self::CATEGORY as $key => $categoryTitle) {
-            $category = new Category();
-            $category->setTitle($categoryTitle);
-            $this->addReference('category_' . $key, $category);
 
-            $manager->persist($category);
-        }
+        $category = new Category();
+        $category->setTitle('Environement');
+        $category->setImage('fixtures/environnement.jpg');
+
+        $this->addReference('category_0', $category);
+
+        $manager->persist($category);
+
+
+
+        $category = new Category();
+        $category->setTitle('Économie');
+        $category->setImage('fixtures/economie.jpg');
+
+        $this->addReference('category_1', $category);
+
+        $manager->persist($category);
+
+
+        $category = new Category();
+        $category->setTitle('Droit');
+        $category->setImage('fixtures/droit1.jpg');
+        $this->addReference('category_2', $category);
+
+        $manager->persist($category);
+
+
+        $category = new Category();
+        $category->setTitle('Professionnel');
+        $category->setImage('fixtures/professionnel.jpg');
+        $this->addReference('category_3', $category);
+
+        $manager->persist($category);
+
+        $category = new Category();
+        $category->setTitle('Personnel');
+        $category->setImage('fixtures/personnel.jpg');
+        $this->addReference('category_4', $category);
+
+        $manager->persist($category);
+
+
+        // foreach (self::CATEGORY as $key => $categoryTitle) {
+        //     $category = new Category();
+        //     $category->setTitle($categoryTitle);
+        //     $this->addReference('category_' . $key, $category);
+        //     $manager->persist($category);
+        // }
 
         $manager->flush();
     }
