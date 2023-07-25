aebe76c1e3837349646564a57fb88d8963dc8bd7
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-07-25 21:42:56 +0200

    fixtures for expert and impated users

--- src/DataFixtures/DecisionFixtures.php
+++ src/DataFixtures/DecisionFixtures.php
@@ -21,12 +21,32 @@ class DecisionFixtures extends Fixture implements DependentFixtureInterface
         for ($i = 0; $i < self::DECISION_NUMBER; $i++) {
             $decision = new Decision();
 
+            $numberOfImpactedUsers = mt_rand(1, 5);
+            $numberOfExpertUsers = mt_rand(1, 5);
+
+            for ($j = 0; $j < $numberOfImpactedUsers; $j++) {
+                $impactedUsers[] = $this->getReference('user_' . $faker->numberBetween(0, 39));
+            }
+
+            for ($j = 0; $j < $numberOfExpertUsers; $j++) {
+                $expertUsers[] = $this->getReference('user_' . $faker->numberBetween(0, 40));
+            }
+
+            foreach ($impactedUsers as $impactedUser) {
+                $decision->addImpactedUser($impactedUser);
+            }
+            foreach ($expertUsers as $expertUser) {
+                $decision->addExpertUser($expertUser);
+            }
+
             $decision->setTitle($faker->realText(50));
             $decision->setStartDate($faker->dateTimeBetween('-3 years', 'now'));
             $decision->setDescription($faker->paragraph(200));
             $decision->setCurrentPlace(array_rand(Decision::STATUS));
             $decision->setCategory($this->getReference('category_' . $faker->numberBetween(0, 4)));
             $decision->setUser($this->getReference('user_' . $faker->numberBetween(0, 40)));
+
+
             $this->addReference('decision_' . $i, $decision);
             $manager->persist($decision);
         }
